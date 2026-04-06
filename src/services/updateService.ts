c5abf24693f791d4371d30fcd28c46c092bd7a4c
sinnaruggggg <sinnaruggggg@gmail.com>
2026-04-06 09:48:05 +0900

    release: 1.3.20

--- src/services/updateService.ts
+++ src/services/updateService.ts
@@ -2,8 +2,8 @@ import {Alert, Linking, Platform} from 'react-native';
 import ReactNativeBlobUtil from 'react-native-blob-util';
 import {isNewerVersion} from './updateVersion';
 
-export const CURRENT_VERSION_CODE = 147;
-export const CURRENT_VERSION_NAME = '1.3.19';
+export const CURRENT_VERSION_CODE = 148;
+export const CURRENT_VERSION_NAME = '1.3.20';
 
 const GITHUB_REPO = 'sinnaruggggg/blockhero';
 const APK_MIME = 'application/vnd.android.package-archive';
